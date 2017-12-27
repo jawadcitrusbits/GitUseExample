5b71fbaf7f5ed0a57ff9da0adafcfae2897e7aa0
zak <zak@ps-Work.local>
2017-12-27 11:49:47 +0500

    after map_feature added

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by zak on 12/27/17.
+//  Copyright © 2017 zak. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
